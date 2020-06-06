8e88f50c46515b889244938e8ddd7ebb7d812a12
heecheolman <msn9975@gmail.com>
2020-06-06 10:13:40 +0900

    fix(브루트포스/블랙잭(2798)/김희철): 소스코드

--- 브루트포스/[2798]블랙잭/김희철/main.py
+++ 브루트포스/[2798]블랙잭/김희철/main.py
@@ -6,9 +6,9 @@ card_numbers = [int(num) for num in input().split()]
 def draw():
     result = 0
     for a_index, a in enumerate(card_numbers):
-        for b_index, b in enumerate(card_numbers):
-            for c_index, c in enumerate(card_numbers):
-                if a_index != b_index and b_index != c_index and c_index != a_index:
+        for b_index, b in enumerate(card_numbers[a_index + 1:]):
+            for c_index, c in enumerate(card_numbers[a_index + 2:]):
+                if a != b and b != c and c != a:
                     blackjack = a + b + c
                     if result < blackjack <= M:
                         result = blackjack
