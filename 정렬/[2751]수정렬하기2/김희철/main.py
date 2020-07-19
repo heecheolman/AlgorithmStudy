642273834fb319c35de1429fdb35423fcf7d74a2
HeeCheolKim <msn9975@gmail.com>
2020-07-20 02:12:10 +0900

    add(정렬/수정렬하기2(2751)/김희철): 퀵소트 배열이용

--- 정렬/[2751]수정렬하기2/김희철/main.py
+++ 정렬/[2751]수정렬하기2/김희철/main.py
@@ -7,45 +7,27 @@ for _ in range(N):
     numbers.append(int(stdin.readline()))
 
 
-def merge_sort(array):
+def quick_sort(array):
     length = len(array)
-    if length == 1:
+    if length <= 1:
         return array
 
-    half_index = length // 2
-    left = array[:half_index]
-    right = array[half_index:]
+    pivot = array[length // 2]
 
-    a = merge_sort(left)
-    b = merge_sort(right)
+    small = []
+    big = []
+    equal = []
 
-    return merge(a, b)
-
-
-def merge(left, right):
-    left_cur = 0
-    right_cur = 0
-    left_end = len(left) - 1
-    right_end = len(right) - 1
-
-    sorted_list = []
-
-    while left_cur <= left_end and right_cur <= right_end:
-        if left[left_cur] < right[right_cur]:
-            sorted_list.append(left[left_cur])
-            left_cur += 1
+    for v in array:
+        if v < pivot:
+            small.append(v)
+        elif v > pivot:
+            big.append(v)
         else:
-            sorted_list.append(right[right_cur])
-            right_cur += 1
+            equal.append(v)
 
-    if left_cur <= left_end:
-        sorted_list = sorted_list + left[left_cur:]
-
-    if right_cur <= right_end:
-        sorted_list = sorted_list + right[right_cur:]
-
-    return sorted_list
+    return quick_sort(small) + equal + quick_sort(big)
 
 
-for value in merge_sort(numbers):
+for value in quick_sort(numbers):
     print(value)
