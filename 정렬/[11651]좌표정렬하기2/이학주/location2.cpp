bf013ed4efb6df1dc58224f30608c0f87c799a26
HakJu <gkrwnrh@gmail.com>
2020-07-31 19:26:52 +0900

    add(정렬/좌표정렬하기2(11651)/이학주): 소스코드

---
+++ 정렬/[11651]좌표정렬하기2/이학주/location2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main () {
+    vector < pair <int, int> > v;
+    vector < pair <int, int> > output;
+    int N, a, b;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> a >> b;
+        v.push_back(pair<int, int>(b, a));
+    }
+    sort(v.begin(), v.end());
+
+    vector < pair <int, int> > ::iterator iter;
+    for (iter = v.begin(); iter != v.end(); iter++) {
+        cout << iter->second << " " << iter->first << "\n";
+    }
+}
